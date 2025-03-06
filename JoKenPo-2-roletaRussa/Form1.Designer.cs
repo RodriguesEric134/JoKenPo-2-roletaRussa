f65877e03fb774dc0ba10b22a55671647c2e88cf
RodriguesEric134 <rm550249@fiap.com.br>
2025-03-06 15:52:34 -0300

    Estou preso nessa parte, deixando salvo para não perder

--- JoKenPo-2-roletaRussa/Form1.Designer.cs
+++ JoKenPo-2-roletaRussa/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace JoKenPo_2_roletaRussa
 {
-    partial class Form1
+    partial class Frm_Principal
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Principal));
+            this.Lbl_ExibeNome = new System.Windows.Forms.Label();
+            this.Lbl_NomeBot = new System.Windows.Forms.Label();
+            this.Opc_Tesoura_Bot = new System.Windows.Forms.PictureBox();
+            this.Opc_Papel_Bot = new System.Windows.Forms.PictureBox();
+            this.Opc_Pedra_Bot = new System.Windows.Forms.PictureBox();
+            this.Opc_Tesoura = new System.Windows.Forms.PictureBox();
+            this.Opc_Papel = new System.Windows.Forms.PictureBox();
+            this.Opc_Pedra = new System.Windows.Forms.PictureBox();
+            this.Lbl_X = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Opc_Tesoura_Bot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Opc_Papel_Bot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Opc_Pedra_Bot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Opc_Tesoura)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Opc_Papel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Opc_Pedra)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Lbl_ExibeNome
+            // 
+            this.Lbl_ExibeNome.AutoSize = true;
+            this.Lbl_ExibeNome.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_ExibeNome.Location = new System.Drawing.Point(12, 13);
+            this.Lbl_ExibeNome.Name = "Lbl_ExibeNome";
+            this.Lbl_ExibeNome.Size = new System.Drawing.Size(67, 21);
+            this.Lbl_ExibeNome.TabIndex = 3;
+            this.Lbl_ExibeNome.Text = "Jogador:";
+            // 
+            // Lbl_NomeBot
+            // 
+            this.Lbl_NomeBot.AutoSize = true;
+            this.Lbl_NomeBot.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_NomeBot.Location = new System.Drawing.Point(344, 13);
+            this.Lbl_NomeBot.Name = "Lbl_NomeBot";
+            this.Lbl_NomeBot.Size = new System.Drawing.Size(33, 21);
+            this.Lbl_NomeBot.TabIndex = 7;
+            this.Lbl_NomeBot.Text = "Bot";
+            // 
+            // Opc_Tesoura_Bot
+            // 
+            this.Opc_Tesoura_Bot.Image = global::JoKenPo_2_roletaRussa.Properties.Resources.tesouras;
+            this.Opc_Tesoura_Bot.Location = new System.Drawing.Point(289, 242);
+            this.Opc_Tesoura_Bot.Name = "Opc_Tesoura_Bot";
+            this.Opc_Tesoura_Bot.Size = new System.Drawing.Size(90, 90);
+            this.Opc_Tesoura_Bot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Opc_Tesoura_Bot.TabIndex = 6;
+            this.Opc_Tesoura_Bot.TabStop = false;
+            // 
+            // Opc_Papel_Bot
+            // 
+            this.Opc_Papel_Bot.Image = global::JoKenPo_2_roletaRussa.Properties.Resources.papel_de_mao;
+            this.Opc_Papel_Bot.Location = new System.Drawing.Point(289, 146);
+            this.Opc_Papel_Bot.Name = "Opc_Papel_Bot";
+            this.Opc_Papel_Bot.Size = new System.Drawing.Size(90, 90);
+            this.Opc_Papel_Bot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Opc_Papel_Bot.TabIndex = 5;
+            this.Opc_Papel_Bot.TabStop = false;
+            // 
+            // Opc_Pedra_Bot
+            // 
+            this.Opc_Pedra_Bot.Image = global::JoKenPo_2_roletaRussa.Properties.Resources.mao;
+            this.Opc_Pedra_Bot.Location = new System.Drawing.Point(289, 50);
+            this.Opc_Pedra_Bot.Name = "Opc_Pedra_Bot";
+            this.Opc_Pedra_Bot.Size = new System.Drawing.Size(90, 90);
+            this.Opc_Pedra_Bot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Opc_Pedra_Bot.TabIndex = 4;
+            this.Opc_Pedra_Bot.TabStop = false;
+            // 
+            // Opc_Tesoura
+            // 
+            this.Opc_Tesoura.Image = global::JoKenPo_2_roletaRussa.Properties.Resources.tesouras;
+            this.Opc_Tesoura.Location = new System.Drawing.Point(12, 242);
+            this.Opc_Tesoura.Name = "Opc_Tesoura";
+            this.Opc_Tesoura.Size = new System.Drawing.Size(90, 90);
+            this.Opc_Tesoura.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Opc_Tesoura.TabIndex = 2;
+            this.Opc_Tesoura.TabStop = false;
+            this.Opc_Tesoura.Click += new System.EventHandler(this.Opc_Tesoura_Click);
+            // 
+            // Opc_Papel
+            // 
+            this.Opc_Papel.Image = global::JoKenPo_2_roletaRussa.Properties.Resources.papel_de_mao;
+            this.Opc_Papel.Location = new System.Drawing.Point(12, 146);
+            this.Opc_Papel.Name = "Opc_Papel";
+            this.Opc_Papel.Size = new System.Drawing.Size(90, 90);
+            this.Opc_Papel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Opc_Papel.TabIndex = 1;
+            this.Opc_Papel.TabStop = false;
+            this.Opc_Papel.Click += new System.EventHandler(this.Opc_Papel_Click);
+            // 
+            // Opc_Pedra
+            // 
+            this.Opc_Pedra.Image = global::JoKenPo_2_roletaRussa.Properties.Resources.mao;
+            this.Opc_Pedra.Location = new System.Drawing.Point(12, 50);
+            this.Opc_Pedra.Name = "Opc_Pedra";
+            this.Opc_Pedra.Size = new System.Drawing.Size(90, 90);
+            this.Opc_Pedra.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Opc_Pedra.TabIndex = 0;
+            this.Opc_Pedra.TabStop = false;
+            this.Opc_Pedra.Click += new System.EventHandler(this.Opc_Pedra_Click);
+            // 
+            // Lbl_X
+            // 
+            this.Lbl_X.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_X.ForeColor = System.Drawing.Color.Red;
+            this.Lbl_X.Location = new System.Drawing.Point(160, 159);
+            this.Lbl_X.Name = "Lbl_X";
+            this.Lbl_X.Size = new System.Drawing.Size(77, 77);
+            this.Lbl_X.TabIndex = 8;
+            this.Lbl_X.Text = "X";
+            // 
+            // Frm_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(393, 344);
+            this.Controls.Add(this.Lbl_X);
+            this.Controls.Add(this.Lbl_NomeBot);
+            this.Controls.Add(this.Opc_Tesoura_Bot);
+            this.Controls.Add(this.Opc_Papel_Bot);
+            this.Controls.Add(this.Opc_Pedra_Bot);
+            this.Controls.Add(this.Lbl_ExibeNome);
+            this.Controls.Add(this.Opc_Tesoura);
+            this.Controls.Add(this.Opc_Papel);
+            this.Controls.Add(this.Opc_Pedra);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Frm_Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jokenpo 2.0 com Roleta Russa";
+            ((System.ComponentModel.ISupportInitialize)(this.Opc_Tesoura_Bot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Opc_Papel_Bot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Opc_Pedra_Bot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Opc_Tesoura)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Opc_Papel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Opc_Pedra)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Opc_Pedra;
+        private System.Windows.Forms.PictureBox Opc_Papel;
+        private System.Windows.Forms.PictureBox Opc_Tesoura;
+        private System.Windows.Forms.Label Lbl_ExibeNome;
+        private System.Windows.Forms.PictureBox Opc_Tesoura_Bot;
+        private System.Windows.Forms.PictureBox Opc_Papel_Bot;
+        private System.Windows.Forms.PictureBox Opc_Pedra_Bot;
+        private System.Windows.Forms.Label Lbl_NomeBot;
+        private System.Windows.Forms.Label Lbl_X;
     }
 }
 
--- JoKenPo-2-roletaRussa/Form1.cs
+++ JoKenPo-2-roletaRussa/Form1.cs
@@ -1,20 +1,214 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace JoKenPo_2_roletaRussa
 {
-    public partial class Form1 : Form
+    public partial class Frm_Principal : Form
     {
-        public Form1()
+        public Frm_Principal()
         {
             InitializeComponent();
+        }
+
+        List<string> opcoesSelecionadas = new List<string>();
+        Random random = new Random();
+
+        private void Opc_Pedra_Click(object sender, EventArgs e)
+        {
+            SelecionarOpcao(Opc_Pedra, "pedra");
+        }
+
+        private void Opc_Papel_Click(object sender, EventArgs e)
+        {
+            SelecionarOpcao(Opc_Papel, "papel");
+        }
+
+        private void Opc_Tesoura_Click(object sender, EventArgs e)
+        {
+            SelecionarOpcao(Opc_Tesoura, "tesoura");
+        }
+
+        private void SelecionarOpcao(PictureBox opcao, string nome)
+        {
+            if (opcoesSelecionadas.Contains(nome)) return;
+
+            opcoesSelecionadas.Add(nome);
+
+            switch (nome)
+            {
+                case "pedra":
+                    opcao.Image = Properties.Resources.mao_circulada;
+                    break;
+                case "papel":
+                    opcao.Image = Properties.Resources.papel_de_mao_circulada;
+                    break;
+                case "tesoura":
+                    opcao.Image = Properties.Resources.tesouras_circulada;
+                    break;
+            }
+
+            if (opcoesSelecionadas.Count == 2)
+            {
+                DefinirImagemExcluida();
+                JogadaBot(); // O Bot joga logo após a seleção do jogador
+            }
+
+            if (opcoesSelecionadas.Count > 2)
+            {
+                ResetarSelecoes();
+            }
+        }
+
+        private void DefinirImagemExcluida()
+        {
+            if (!opcoesSelecionadas.Contains("pedra"))
+                Opc_Pedra.Image = Properties.Resources.mao_cx;
+            if (!opcoesSelecionadas.Contains("papel"))
+                Opc_Papel.Image = Properties.Resources.papel_de_mao_cx;
+            if (!opcoesSelecionadas.Contains("tesoura"))
+                Opc_Tesoura.Image = Properties.Resources.tesouras_cx;
+        }
+
+        private void JogadaBot()
+        {
+            string[] opcoes = { "pedra", "papel", "tesoura" };
+
+            // Escolher duas opções aleatórias para o Bot
+            List<string> opcoesBot = opcoes.OrderBy(x => random.Next()).Take(2).ToList();
+
+            // Definir imagens para as opções selecionadas
+            foreach (string opcao in opcoesBot)
+            {
+                switch (opcao)
+                {
+                    case "pedra":
+                        Opc_Pedra_Bot.Image = Properties.Resources.mao_circulada;
+                        break;
+                    case "papel":
+                        Opc_Papel_Bot.Image = Properties.Resources.papel_de_mao_circulada;
+                        break;
+                    case "tesoura":
+                        Opc_Tesoura_Bot.Image = Properties.Resources.tesouras_circulada;
+                        break;
+                }
+            }
+
+            // Definir imagem "X" para a opção excluída
+            string excluida = opcoes.Except(opcoesBot).First();
+            switch (excluida)
+            {
+                case "pedra":
+                    Opc_Pedra_Bot.Image = Properties.Resources.mao_cx;
+                    break;
+                case "papel":
+                    Opc_Papel_Bot.Image = Properties.Resources.papel_de_mao_cx;
+                    break;
+                case "tesoura":
+                    Opc_Tesoura_Bot.Image = Properties.Resources.tesouras_cx;
+                    break;
+            }
+        }
+
+        private void ResetarSelecoes()
+        {
+            opcoesSelecionadas.Clear();
+            Opc_Pedra.Image = null;
+            Opc_Papel.Image = null;
+            Opc_Tesoura.Image = null;
+            Opc_Pedra_Bot.Image = null;
+            Opc_Papel_Bot.Image = null;
+            Opc_Tesoura_Bot.Image = null;
+        }
+
+        // Função para fazer o jogador escolher uma das duas opções selecionadas
+        private void EscolherOpcaoFinal(PictureBox opcao, string nome)
+        {
+            // Remover as imagens das opções não escolhidas
+            if (nome == "pedra")
+            {
+                Opc_Pedra.Image = Properties.Resources.mao;
+                Opc_Papel.Image = null;
+                Opc_Tesoura.Image = null;
+            }
+            else if (nome == "papel")
+            {
+                Opc_Papel.Image = Properties.Resources.papel_de_mao;
+                Opc_Pedra.Image = null;
+                Opc_Tesoura.Image = null;
+            }
+            else if (nome == "tesoura")
+            {
+                Opc_Tesoura.Image = Properties.Resources.tesouras;
+                Opc_Pedra.Image = null;
+                Opc_Papel.Image = null;
+            }
+
+            // Agora, o bot escolhe aleatoriamente uma opção final entre as opções que ele selecionou
+            EscolherOpcaoFinalBot();
+        }
+
+        // Função para o bot escolher aleatoriamente uma das opções finais
+        private void EscolherOpcaoFinalBot()
+        {
+            string[] opcoes = { "pedra", "papel", "tesoura" };
+
+            // O bot vai escolher aleatoriamente uma das opções que ele selecionou
+            string opcaoEscolhida = opcoes.OrderBy(x => random.Next()).First();
+
+            // Exibir a imagem do bot conforme a escolha final
+            if (opcaoEscolhida == "pedra")
+            {
+                Opc_Pedra_Bot.Image = Properties.Resources.mao;
+                Opc_Papel_Bot.Image = null;
+                Opc_Tesoura_Bot.Image = null;
+            }
+            else if (opcaoEscolhida == "papel")
+            {
+                Opc_Papel_Bot.Image = Properties.Resources.papel_de_mao;
+                Opc_Pedra_Bot.Image = null;
+                Opc_Tesoura_Bot.Image = null;
+            }
+            else if (opcaoEscolhida == "tesoura")
+            {
+                Opc_Tesoura_Bot.Image = Properties.Resources.tesouras;
+                Opc_Pedra_Bot.Image = null;
+                Opc_Papel_Bot.Image = null;
+            }
+
+            // Após a escolha, podemos fazer a comparação
+            CompararEscolhas();
+        }
+
+        private void CompararEscolhas()
+        {
+            // Aqui você compara as opções do jogador e do bot
+            string jogadorEscolha = opcoesSelecionadas.First(); // Como temos apenas uma escolha final por jogador
+            string botEscolha = string.Empty;
+
+            if (Opc_Pedra_Bot.Image != null) botEscolha = "pedra";
+            else if (Opc_Papel_Bot.Image != null) botEscolha = "papel";
+            else if (Opc_Tesoura_Bot.Image != null) botEscolha = "tesoura";
+
+            string resultado = string.Empty;
+
+            if (jogadorEscolha == botEscolha)
+            {
+                resultado = "Empate!";
+            }
+            else if ((jogadorEscolha == "pedra" && botEscolha == "tesoura") ||
+                     (jogadorEscolha == "tesoura" && botEscolha == "papel") ||
+                     (jogadorEscolha == "papel" && botEscolha == "pedra"))
+            {
+                resultado = "Você Ganhou!";
+            }
+            else
+            {
+                resultado = "Você Perdeu!";
+            }
+
+            MessageBox.Show(resultado);
         }
     }
 }
